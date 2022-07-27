e6e0cb41cb53045dc2306f4400970964a53c98ec
Irena <irena.laemmerer@gmail.com>
2022-07-27 09:45:22 +0200

    add remix icons

--- stories/Icon.stories.tsx
+++ stories/Icon.stories.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react-native';
 
-import { Icon as Ic } from '../src';
+import { Icon as Ic, theme } from '../src';
 
 const IconMeta: ComponentMeta<typeof Ic> = {
   title: 'Icons',
   component: Ic,
+  argTypes: {
+    color: {
+      name: 'color',
+      // type: 'select',
+      // options: [
+      //   theme.colors.icons.dark,
+      //   theme.colors.icons.light,
+      //   theme.colors.icons.link,
+      // ],
+      description: 'icon color',
+      control: {
+        type: 'color'
+      }
+    },
+  },
 };
 
 export default IconMeta;
@@ -13,4 +28,4 @@ export default IconMeta;
 const Template: ComponentStory<typeof Ic> = (args) => <Ic {...args} />;
 
 export const Icon = Template.bind({});
-Icon.args = { size: '5', name: 'Delete' };
+Icon.args = { size: '6', color: theme.colors.icons.link, name: 'Attachment' };
