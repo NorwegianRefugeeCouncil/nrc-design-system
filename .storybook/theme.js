657bd28da4e0ad5a226528c0be41e916ef711475
Irena Laemmerer <irena.lammerer@nrc.no>
2022-09-06 13:34:41 +0200

    theming storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,10 +1,27 @@
 import { create } from '@storybook/theming';
-import logo from '../static/nrc_logo.png'
-
+import { colorTokens } from '../src/tokens/colors';
+import logo from '../static/nrc_logo.png';
 
 export default create({
   base: 'light',
   brandTitle: 'NRC Design system',
   brandUrl: 'https://www.nrc.no/',
-  brandImage: logo
+  brandImage: logo,
+
+  colorPrimary: colorTokens.primary500,
+  colorSecondary: colorTokens.secondary500,
+
+  appBg: colorTokens.themeLight,
+  appContentBg: colorTokens.white,
+  appBorderColor: colorTokens.neutral200,
+  appBorderRadius: 4,
+
+  barTextColor: colorTokens.neutral400,
+  barSelectedColor: colorTokens.primary500,
+  barBg: colorTokens.white,
+
+  inputBg: colorTokens.white,
+  inputBorder: colorTokens.primary500,
+  inputTextColor: colorTokens.neutral100,
+  inputBorderRadius: 4,
 });
