040498baad1ed112407392a8eb896a2661a0c977
Irena Laemmerer <irena.lammerer@nrc.no>
2022-09-19 11:53:45 +0200

    overwrite NB exports with our own, bundle exports better

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as NativeBase from 'native-base';
+import * as Components from './components';
+
+export const components = Object.assign({}, NativeBase, Components);
 export { default as theme } from './themes';
 export { default as tokens } from './tokens';
-export * from './components';
