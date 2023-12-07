d4b923bd18d900b664f20adaf04183e68e8e7751
David Figueroa <david.figueroa@nrc.no>
2023-12-07 14:23:57 +0100

    fix(types): defined type for components

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as NativeBase from 'native-base';
-
 import * as Components from './components';
 
-export const components = { ...NativeBase, ...Components };
+type ComponentsType = typeof NativeBase & typeof Components;
+
+export const components: ComponentsType = { ...NativeBase, ...Components } as ComponentsType;
 
 export { default as theme } from './themes';
 export { default as tokens } from './tokens';
